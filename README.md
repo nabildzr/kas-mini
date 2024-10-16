7a86bd8259b6463ce51331b2ca437a3dfb476a09
Putra2078 <alphaaldiansyah@gmail.com>
2024-10-16 14:03:36 +0700

    first step

---
+++ README.md
@@ -0,0 +1,3 @@
+# kas_mini_flutter_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/kas_mini_flutter_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kas_mini_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
